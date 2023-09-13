5e1a42e6c05ac354fca80fcaafe3ec02a676477d
adielx2890x <adielx2890x@gmail.com>
2023-09-13 11:52:27 -0600

    Ajai

--- pokedex-app/src/Pokedex.js
+++ pokedex-app/src/Pokedex.js
@@ -4,28 +4,56 @@ import { getPokemonList } from "./api";
 function Ajai() {
   const [pokemonList, setPokemonList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [nextPageUrl, setNextPageUrl] = useState("");
+  const [previousPageUrl, setPreviousPageUrl] = useState("");
 
   useEffect(() => {
     async function fetchData() {
       const url = `https://pokeapi.co/api/v2/pokemon`;
+
       const data = await getPokemonList(url);
+
       setIsLoading(false);
+      setCurrentPage(1);
       if (data) {
         setPokemonList(data.array);
+        setNextPageUrl(data.next);
       }
     }
 
     fetchData();
   }, []);
 
+  async function next(url) {
+    setIsLoading(true);
+    const response = await getPokemonList(url);
+    //console.log(response);
+    const data = response.array;
+    setPokemonList(data);
+    const next = response.next;
+    setNextPageUrl(next);
+    setIsLoading(false);
+    //previous previous
+    setPreviousPageUrl(response.previous);
 
-  function next(){
-
-    alert("hola")
+    // alert("hola")
   }
 
-  function back(){
-    alert("AJAI")
+  async function previous(url) {
+    setIsLoading(true);
+    console.log("previus", url);
+    const response = await getPokemonList(url);
+    const data = response.array;
+    //update url
+    const next = response.next;
+    setNextPageUrl(next);
+    //
+    const previous = response.previous;
+    setPreviousPageUrl(previous);
+    //
+    setPokemonList(data);
+    setIsLoading(false);
   }
   //
 
@@ -52,21 +80,22 @@ function Ajai() {
               </li>
             ))}
             <div id="boxButton">
-              <button onClick={back} id="vegetaButton" className="buttons">
+              <button
+                onClick={() => previous(previousPageUrl)}
+                id="vegetaButton"
+                className="buttons"
+              ></button>
 
-        
-              </button>
-
-              <button onClick={next} id="gokuButton" className="buttons">
-               
-              </button>
-
-
+              <button
+                onClick={() => next(nextPageUrl)}
+                id="gokuButton"
+                className="buttons"
+              ></button>
 
               <form>
                 <input type="text"></input>
                 <button>Buscar</button>
-                </form>              
+              </form>
             </div>
           </ul>
         </div>
